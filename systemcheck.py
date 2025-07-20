cc5cd310f54174ab1e5c8b579901530455d44ffe
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-07-20 13:27:32 -0700

    added a line for ping to a localhost

--- systemcheck.py
+++ systemcheck.py
@@ -7,3 +7,6 @@ def get_cpu_usage():
 def get_available_memory_mb():
     return psutil.virtual_memory().available / (1024 ** 2)
 
+def ping_localhost():
+    result = subprocess.run(["ping", "-c", "1", "127.0.0.1"], capture_output=True)
+    return result.returncode == o
